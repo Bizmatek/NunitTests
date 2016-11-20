6b07c926f54ea3f017dbd6cc709a18e8c38480eb
bizmatek <bizmatek@mail.ru>
2016-11-20 23:17:06 +0300

    2

--- NunitTests/Class1.cs
+++ NunitTests/Class1.cs
@@ -22,25 +22,34 @@ namespace GeometricFigures
     {
 
         static object[] figureCases = {
-               new object[] { 5, 7, 3, 7, 4 },
-               new object[] { 2, 9, 2, 1, 5 },
-               new object[] { 1, 3, 7, 4, 5 }
+               new object[] { 5 }, //, 7, 3, 3,  3},
+               new object[] { 2 },//, 9, 2, 1, 5 },
+               new object[] { 1 },//, 3, 7, 4, 5 }
         };
 
-        [Test]
+        static object[] circleCases = {
+                new object[] { 5},
+                new object[] { 7},
+                new object[] { 8}
+        };
 
-        public void test1() {
+        [Test,  TestCaseSource("circleCases")]
 
-            Circle a = new Circle(5);
-            Assert.AreEqual(a.getAreaCircle(), (Math.PI * Math.Pow(5, 2)));
+        public void test1(int rad) {
+
+            Circle a = new Circle(rad);
+            Assert.AreEqual(a.getAreaCircle(), (Math.PI * Math.Pow(rad, 2)));
         }
 
-        [TestFixtureSetUp, TestCaseSource("figureCases")]
-        public void initObjects(int circleRadius, int squareSide, int triangleSide1, int triangleSide2, int triangleSide3 ) {
-            Circle circle = new Circle(circleRadius);
-            Square square = new Square(squareSide);
-            Triangle triangle = new Triangle(triangleSide1, triangleSide2, triangleSide3);
-        }
+         //[TestFixtureSetUp, TestCaseSource("figureCases")]
+        //  public void initObjects(int circleRadius, int squareSide, int triangleSide1, int triangleSide2, int triangleSide3 ) {
+        //public void initObjects(int circleRadius) {
+          //  Circle circle = new Circle(circleRadius);
+       // }
+        //     Circle circle = new Circle(circleRadius);
+       //     Square square = new Square(squareSide);
+      //      Triangle triangle = new Triangle(triangleSide1, triangleSide2, triangleSide3);
+      //  }
 
     }
 
