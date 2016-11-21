60f302d2e08898b738ecf251c806c89a50722250
Pavel.Loginov <Pavel.Loginov@NPC-99>
2016-11-21 15:52:22 +0300

    3

--- NunitTests/Class1.cs
+++ NunitTests/Class1.cs
@@ -15,41 +15,146 @@ using NUnit.Framework;
 namespace GeometricFigures
 {
 
+ 
     [TestFixture]
 
     public class NUnitTests
 
     {
 
-        static object[] figureCases = {
-               new object[] { 5 }, //, 7, 3, 3,  3},
-               new object[] { 2 },//, 9, 2, 1, 5 },
-               new object[] { 1 },//, 3, 7, 4, 5 }
-        };
-
         static object[] circleCases = {
-                new object[] { 5},
-                new object[] { 7},
-                new object[] { 8}
+            new int[] {4},
+            new int[] {0},
+            new int[] {-5}
+               
         };
 
-        [Test,  TestCaseSource("circleCases")]
+        static object[] SquareCases = {
+            new int[] { 7 },
+            new int[] { 0 },
+            new int[] { -3}
+        };
 
-        public void test1(int rad) {
+        static object[] TriangleCases = {
+            new int[] { 3, 4, 1},
+            new int[] { -14, -4, -1}
+        };
 
-            Circle a = new Circle(rad);
-            Assert.AreEqual(a.getAreaCircle(), (Math.PI * Math.Pow(rad, 2)));
+
+        /*[Test, TestCaseSource("circleCases")]
+        public void isAreaCircleNotNull(int r) {
+            Circle a = new Circle(r);
+            Assert.IsTrue(a.getAreaCircle() != 0);
+        }*/
+
+
+
+        /*[Test, TestCaseSource("circleCases")]
+        public void isLengthCircleMoreThenNull(int r)
+        {
+            Circle a = new Circle(r);
+            Assert.IsTrue(a.getLengthCircle()  > 0);
+        }*/
+
+
+        [Test, TestCaseSource("circleCases")]
+        public void isCircleArgumentsCorrect(int r)
+        {
+            Circle a = new Circle(r);
+            Assert.IsTrue(r > 0);
         }
 
-         //[TestFixtureSetUp, TestCaseSource("figureCases")]
-        //  public void initObjects(int circleRadius, int squareSide, int triangleSide1, int triangleSide2, int triangleSide3 ) {
-        //public void initObjects(int circleRadius) {
-          //  Circle circle = new Circle(circleRadius);
-       // }
-        //     Circle circle = new Circle(circleRadius);
-       //     Square square = new Square(squareSide);
-      //      Triangle triangle = new Triangle(triangleSide1, triangleSide2, triangleSide3);
-      //  }
+
+        [Test, TestCaseSource("circleCases")]
+        public void isAreaCircleCorrect(int r)
+        {
+            Circle a = new Circle(r);
+            Assert.AreEqual(a.getAreaCircle(), (Math.PI * Math.Pow(r, 2)));
+            Assert.IsTrue(r > 0);
+        }
+
+
+
+        [Test, TestCaseSource("circleCases")]
+        public void isLengthCircleCorrect(int r)
+        {
+            Circle a = new Circle(r);
+            Assert.AreEqual(a.getLengthCircle(), (Math.PI * (r * 2)));
+            Assert.IsTrue(r > 0);
+        }
+
+
+        //square
+
+        [Test, TestCaseSource("SquareCases")]
+        public void isSquareAreaCorrect(int side) {
+            Square s = new Square(side);
+            Assert.AreEqual(s.getAreaSquare(), Math.Pow(side, 2));
+            Assert.IsTrue(side > 0);
+        }
+
+
+        [Test, TestCaseSource("SquareCases")]
+        public void isSquareLengthCorrect(int side)
+        {
+            Square s = new Square(side);
+            Assert.AreEqual(s.getLengthSquare(), side*4);
+            Assert.IsTrue(s.getLengthSquare() > 0);
+        }
+
+
+
+        [Test, TestCaseSource("SquareCases")]
+        public void isSquareArgumentsCorrect(int side)
+        {
+            Square s = new Square(side);
+            Assert.IsTrue(side > 0);
+        }
+
+        /*[Test, TestCaseSource("SquareCases")]
+        public void isSquareAreaMoreThenNull(int side)
+        {
+            Square s = new Square(side);
+            Assert.IsTrue(s.getAreaSquare() > 0);
+        }
+
+
+        [Test, TestCaseSource("SquareCases")]
+        public void isSquareLengthMoreThenNull(int side)
+        {
+            Square s = new Square(side);
+            Assert.IsTrue(s.getLengthSquare() > 0);
+        }*/
+
+
+        // triangle
+
+
+        [Test, TestCaseSource("TriangleCases")]
+        public void isTriangleLengthCorrect(int side1, int side2, int side3)
+        {
+            Triangle t = new Triangle(side1, side2, side3);
+            Assert.AreEqual(t.getLengthTriangle(), (side1 + side2 + side3));
+            Assert.IsTrue(t.getLengthTriangle() > 0);
+        }
+
+        [Test, TestCaseSource("TriangleCases")]
+        public void isTriangleAreaCorrect(int side1, int side2, int side3)
+        {
+            Triangle t = new Triangle(side1, side2, side3);
+            double p = (side1 + side2 + side3) / 2;
+            double s = Math.Sqrt(p*(p-side1)*(p-side2)*(p-side3));
+            Assert.AreEqual(t.getAreaTriangle(), s, 0.0001);
+            Assert.IsTrue((side1 > 0) && (side2 > 0) && (side3 > 0));
+
+        }
+
+        [Test, TestCaseSource("TriangleCases")]
+        public void isTriangleArgumentsMoreThenNull(int side1, int side2, int side3)
+        {
+            Triangle t = new Triangle(side1, side2, side3);
+            Assert.IsTrue((side1 > 0) && (side2 > 0) && (side3 > 0));
+        }
 
     }
 
