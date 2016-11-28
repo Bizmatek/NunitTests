c341e0bf1f0d27f2040c843d8c361555bd0ba73a
Pavel.Loginov <Pavel.Loginov@NPC-99>
2016-11-28 13:18:03 +0300

    3

--- NunitTests/Class1.cs
+++ NunitTests/Class1.cs
@@ -22,40 +22,44 @@ namespace GeometricFigures
 
     {
 
-        static object[] circleCases = {
-            new int[] {4},
-            new int[] {0},
-            new int[] {-5}
-               
-        };
+        static int[] circleCases = {-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
 
-        static object[] SquareCases = {
-            new int[] { 7 },
-            new int[] { 0 },
-            new int[] { -3}
-        };
+        static int[] SquareCases = { -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };
 
         static object[] TriangleCases = {
-            new int[] { 3, 4, 1},
-            new int[] { -14, -4, -1}
+            new int[] {-1, -1, -1},
+            new int[] { 0, 0, 0},
+            new int[] { 1, 1, 1},
+            new int[] { 2, 2, 2},
+            new int[] { 3, 3, 3},
+            new int[] { 4, 4, 4},
+            new int[] { 5, 5, 5},
+            new int[] { 6, 6, 6},
+            new int[] { 7, 7, 7},
+            new int[] { 8, 8, 8},
+            new int[] { 9, 9, 9},
+            new int[] { 10, 10, 10},
+            new int[] { 11, 11, 11},
+            new int[] { 12, 12, 12},
+            new int[] { 13, 13, 13},
+            new int[] { 14, 14, 14},
+            new int[] { 15, 15, 15},
+            new int[] { 16, 16, 16},
+            new int[] { 17, 17, 17},
+            new int[] { 18, 18, 18},
+            new int[] { 19, 19, 19},
+            new int[] { 20, 20, 20},
+            new int[] { 21, 21, 21},
+            new int[] { 22, 22, 22},
+            new int[] { 23, 23, 23},
+            new int[] { 24, 24, 24},
+            new int[] { 25, 25, 25},
+            new int[] { 26, 26, 26},
+            new int[] { 27, 27, 27},
+            new int[] { 28, 28, 28},
+            new int[] { 29, 29, 29},
+            new int[] { 30, 30, 30},
         };
-
-
-        /*[Test, TestCaseSource("circleCases")]
-        public void isAreaCircleNotNull(int r) {
-            Circle a = new Circle(r);
-            Assert.IsTrue(a.getAreaCircle() != 0);
-        }*/
-
-
-
-        /*[Test, TestCaseSource("circleCases")]
-        public void isLengthCircleMoreThenNull(int r)
-        {
-            Circle a = new Circle(r);
-            Assert.IsTrue(a.getLengthCircle()  > 0);
-        }*/
-
 
         [Test, TestCaseSource("circleCases")]
         public void isCircleArgumentsCorrect(int r)
@@ -110,22 +114,6 @@ namespace GeometricFigures
             Square s = new Square(side);
             Assert.IsTrue(side > 0);
         }
-
-        /*[Test, TestCaseSource("SquareCases")]
-        public void isSquareAreaMoreThenNull(int side)
-        {
-            Square s = new Square(side);
-            Assert.IsTrue(s.getAreaSquare() > 0);
-        }
-
-
-        [Test, TestCaseSource("SquareCases")]
-        public void isSquareLengthMoreThenNull(int side)
-        {
-            Square s = new Square(side);
-            Assert.IsTrue(s.getLengthSquare() > 0);
-        }*/
-
 
         // triangle
 
